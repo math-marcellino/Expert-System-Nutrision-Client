b415f8a37f26e18a80ea6785744777e1213a5099
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-11-21 22:44:04 +0700

    feat: make admin sidebar component

---
+++ src/Components/AdminSidebar.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+function AdminSidebar() {
+    return(
+        <div className="relative min-h-screen bg-green-300 w-1/5">
+            <h1 className="text-white font-bold text-3xl text-center py-5">Nutrition Admin Dashboard</h1>
+
+            <Link to="/admin-dashboard/makanan">
+                <div className="w-full hover:bg-green-400 py-5 cursor-pointer">
+                    <h1 className="text-white text-center text-2xl font-bold">Makanan</h1>
+                </div>
+            </Link>
+            
+            <Link to="/admin-dashboard/larangan">
+                <div className="w-full hover:bg-green-400 py-5 cursor-pointer">
+                    <h1 className="text-white text-center text-2xl font-bold">Larangan Makanan (Obesitas)</h1>
+                </div>
+            </Link>
+        </div>
+    );
+}
+
+export default AdminSidebar;
