1692800d31425ebc8498c2b86c4e597dc47fef82
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-11-21 22:45:34 +0700

    fix: add routing for admin pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Home from './pages/Home';
 import HasilDiagnosa from './pages/HasilDiagnosa';
 import FormDiagnosa from './pages/FormDiagnosa';
 import { Route, Routes } from 'react-router-dom';
+import AdminLogin from './pages/AdminLogin';
+import MakananCRUD from './pages/MakananCRUD';
+import LaranganCrud from './pages/LaranganCRUD';
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/hasil-diagnosa' element={<HasilDiagnosa />} />
         <Route path="/form-diagnosa" element={<FormDiagnosa />} />
+        <Route path="/admin-login" element={<AdminLogin />}/>
+        <Route path="/admin-dashboard/" element={<MakananCRUD/>} />
+        <Route path="/admin-dashboard/makanan" element={<MakananCRUD/>} />
+        <Route path="/admin-dashboard/larangan" element={<LaranganCrud/>} />
       </Routes>
     </div>
   );
