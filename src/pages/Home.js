62889539268baf75f967eaed6d8c2dfe239fd872
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-11-21 22:45:21 +0700

    fix: update admin button routing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,9 @@ function Home() {
         <div>
             <div className="w-4/5 absolute">
                 <h1 className="text-3xl text-green-500 font-extrabold m-5 inline">Expert System</h1>
-                <button className="my-5 px-6 py-2 bg-green-400 hover:bg-green-500 transition duration-300 text-white text-lg font-bold rounded-full inline ">Admin Login</button>
+                <Link to="admin-login">
+                    <button className="my-5 px-6 py-2 bg-green-400 hover:bg-green-500 transition duration-300 text-white text-lg font-bold rounded-full inline ">Admin Login</button>
+                </Link>
             </div>
             <div className="grid grid-cols-7 w-4/5 m-auto">
                 <div className="col-span-4 flex h-screen justify-center items-center">
