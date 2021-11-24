fa4e5343d530ebc33651c5542cf814f1e8c7b953
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-11-24 17:49:28 +0700

    fix: fix admin display

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useRef, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, Navigate } from 'react-router-dom'
 
 function AdminLogin() {
 
@@ -28,31 +28,39 @@ function AdminLogin() {
 
         
     }
-    return (
-        <div className="flex h-screen justify-center items-center m-auto bg-green-300">
-            <form onSubmit={handleSubmit} className="px-5 py-10 rounded-xl bg-white shadow-lg">
-                <h1 className="text-3xl font-bold text-center mb-10">Admin Login</h1>
 
-                <label htmlFor="username" className="mb-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Username</label>
-                <input ref={username} name="username" type="text" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>
-                
-                <label htmlFor="password" className="inline-block text-right w-1/6 font-bold text-xl mx-5">Password</label>
-                <input ref={password} name="password" type="password" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>
-                <br />
-                <br />
-                {message && (
+    if(sessionStorage.getItem("token")){
+        return(
+            <Navigate to="/admin-dashboard"/>
+        )
+    } else {
+        return (
+            <div className="flex h-screen justify-center items-center m-auto bg-green-300">
+                <form onSubmit={handleSubmit} className="px-5 py-10 rounded-xl bg-white shadow-lg">
+                    <h1 className="text-3xl font-bold text-center mb-10">Admin Login</h1>
+    
+                    <label htmlFor="username" className="mb-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Username</label>
+                    <input ref={username} name="username" type="text" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>
+                    
+                    <label htmlFor="password" className="inline-block text-right w-1/6 font-bold text-xl mx-5">Password</label>
+                    <input ref={password} name="password" type="password" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>
+                    <br />
+                    <br />
+                    {message && (
+                        <div className="text-center">
+                            <span className="text-red-500 mb-5">{message}</span>
+                            <br />
+                            <br />
+                        </div>
+                    )}
                     <div className="text-center">
-                        <span className="text-red-500 mb-5">{message}</span>
-                        <br />
-                        <br />
+                        <button type="submit" className="px-5 py-2 text-lg bg-green-400 text-white font-bold rounded-md">Login</button>
                     </div>
-                )}
-                <div className="text-center">
-                    <button type="submit" className="px-5 py-2 text-lg bg-green-400 text-white font-bold rounded-md">Login</button>
-                </div>
-            </form>
-        </div>
-    );
+                </form>
+            </div>
+        );
+    }
+    
 }
 
 export default AdminLogin;
--- src/pages/AdminRegister.js
+++ src/pages/AdminRegister.js
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, Navigate } from 'react-router-dom';
 import { useRef, useState } from 'react';
 import axios from 'axios';
 
@@ -29,32 +29,39 @@ function AdminRegister () {
         }
     }
 
-    return(
-        <div className="flex h-screen justify-center items-center m-auto bg-green-300">
-            <form onSubmit={handleSubmit} className="px-5 py-10 rounded-xl bg-white shadow-lg">
-                <h1 className="text-3xl font-bold text-center mb-10">Admin Register</h1>
-
-                <label htmlFor="username" className="mb-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Username</label>
-                <input ref={username} name="username" type="text" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>
-                
-                <label htmlFor="password" className="inline-block text-right w-1/6 font-bold text-xl mx-5">Password</label>
-                <input ref={password} name="password" type="password" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>
-                <br />
-                <br />
-                {message && (
+    if(sessionStorage.getItem("token")){
+        return(
+            <Navigate to="/admin-dashboard"/>
+        )
+    } else {
+        return(
+            <div className="flex h-screen justify-center items-center m-auto bg-green-300">
+                <form onSubmit={handleSubmit} className="px-5 py-10 rounded-xl bg-white shadow-lg">
+                    <h1 className="text-3xl font-bold text-center mb-10">Admin Register</h1>
+    
+                    <label htmlFor="username" className="mb-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Username</label>
+                    <input ref={username} name="username" type="text" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>
+                    
+                    <label htmlFor="password" className="inline-block text-right w-1/6 font-bold text-xl mx-5">Password</label>
+                    <input ref={password} name="password" type="password" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>
+                    <br />
+                    <br />
+                    {message && (
+                        <div className="text-center">
+                            <span className="text-red-500 mb-5">{message}</span>
+                            <br />
+                            <br />
+                        </div>
+                    )}
                     <div className="text-center">
-                        <span className="text-red-500 mb-5">{message}</span>
-                        <br />
-                        <br />
+                        <button type="submit" className="px-5 py-2 text-lg bg-green-400 text-white font-bold rounded-md">Register</button>
                     </div>
-                )}
-                <div className="text-center">
-                    <button type="submit" className="px-5 py-2 text-lg bg-green-400 text-white font-bold rounded-md">Register</button>
-                </div>
-                
-            </form>
-        </div>
-    )
+                    
+                </form>
+            </div>
+        );
+    }
+    
 }
 
 export default AdminRegister;
