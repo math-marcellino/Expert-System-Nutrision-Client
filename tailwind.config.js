fe39c8c34855650e40ea56f0613b25433ebf0671
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-11-24 17:31:36 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   purge: {
-    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
-    // These options are passed through directly to PurgeCSS
+    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/Components/**/*.{js,jsx,ts,tsx}"]
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
