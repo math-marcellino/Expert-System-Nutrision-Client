6bc0980be21428a3475959847a05d31efc3eb034
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-11-24 16:48:44 +0700

    fix: add admin login & register functionality

--- src/pages/AdminRegister.js
+++ src/pages/AdminRegister.js
@@ -1,37 +1,38 @@
-import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 import { useRef, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
-function AdminLogin() {
-
-    const [message, setMessage] = useState();
+function AdminRegister () {
     const navigate = useNavigate();
 
     const username = useRef();
     const password = useRef();
 
+    const [message, setMessage] = useState('');
+
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const dataLogin = {
+        const registerData = {
             Nama_Admin: username.current.value,
             Password_Admin: password.current.value
         }
-        
-        const res = await axios.post('https://expert-system-nutrition.herokuapp.com/api/admin/acc/signin', dataLogin);
-        console.log(res);
-        if(res.data.login){
-            sessionStorage.setItem("token", res.data.token);
-            navigate('/admin-dashboard', { replace: true });
-        } else{
-            setMessage(res.data.message);
-        }
 
-        
+
+        const res = await axios.post('https://expert-system-nutrition.herokuapp.com/api/admin/acc/signup', registerData);
+
+        console.log(res)
+
+        if(res.data.message == "maaf username telah terdaftar silahkan gunakan username yang baru"){
+            setMessage(res.data.message);
+        } else{
+            navigate('/admin-login', { replace: true });
+        }
     }
-    return (
+
+    return(
         <div className="flex h-screen justify-center items-center m-auto bg-green-300">
             <form onSubmit={handleSubmit} className="px-5 py-10 rounded-xl bg-white shadow-lg">
-                <h1 className="text-3xl font-bold text-center mb-10">Admin Login</h1>
+                <h1 className="text-3xl font-bold text-center mb-10">Admin Register</h1>
 
                 <label htmlFor="username" className="mb-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Username</label>
                 <input ref={username} name="username" type="text" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>
@@ -48,11 +49,12 @@ function AdminLogin() {
                     </div>
                 )}
                 <div className="text-center">
-                    <button type="submit" className="px-5 py-2 text-lg bg-green-400 text-white font-bold rounded-md">Login</button>
+                    <button type="submit" className="px-5 py-2 text-lg bg-green-400 text-white font-bold rounded-md">Register</button>
                 </div>
+                
             </form>
         </div>
-    );
+    )
 }
 
-export default AdminLogin;
+export default AdminRegister;
