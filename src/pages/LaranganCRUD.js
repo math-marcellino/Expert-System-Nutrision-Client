2ded55d105514ca6303014bbf90ac8f77a18265b
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-11-24 16:49:30 +0700

    fix: add restriction to non-admin user

--- src/pages/LaranganCRUD.js
+++ src/pages/LaranganCRUD.js
@@ -48,57 +48,67 @@ function LaranganCrud() {
 
         window.location.href = "/admin-dashboard/larangan";
     }
-    
-    return(
-        <div className="flex">
-            <AdminSidebar />
-            <div className="m-auto p-10">
-                <table className="border-collapse shadow-lg">
-                    <thead className="bg-green-400 text-white">
-                        <tr>
-                            <th className="py-5 px-10">ID Larangan Makanan</th>
-                            <th className="py-5 px-10">Nama Makanan</th>
-                            <th className="py-5 px-10">Action</th>
-                        </tr>
-                    </thead>
-                    {dataLarangan && (
-                        <tbody>
-                            {dataLarangan.map((larangan) => (
-                                <tr className="text-center">
-                                    <td className="p-3 border-b-2">{larangan._id}</td>
-                                    <td className="p-3 border-b-2">{larangan.Makanan.Nama_Makanan}</td>
-                                    <td className="py-3 px-5 border-b-2">
-                                        <button className="p-2 bg-red-500 hover:bg-red-600 text-white rounded-md" onClick={() => {deleteLarangan(larangan._id)}}>Delete</button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    )}
-                </table>
-                <button className="float-right bg-green-400 p-3 text-white font-bold rounded-md mt-5" onClick={fetchMakanan}>Tambah Larangan</button>
-            </div>
-            <Modal isOpen={modalIsOpened} onRequestClose={() => setModalIsOpened(false)}>
-                <button className="p-3 bg-red-500 text-white rounded-md float-right" onClick={() => setModalIsOpened(false)}>Close</button>
 
-                <h1 className="text-3xl font-bold text-black text-center mt-16">Tambah Larangan Makanan (Obesitas)</h1>
-
-                <form onSubmit={(e) => addLarangan(e)}>
-                    <label htmlFor="laranganMakanan" className="mt-12 inline-block text-right w-1/6 font-bold text-xl mx-5">Nama Makanan</label>
-                        {dataMakanan && (
-                            <select ref={laranganTambahan} name="laranganMakanan" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3">
-                                
-                                {dataMakanan.map((makanan) => (
-                                    <option value={makanan._id}>{makanan.Nama_Makanan}</option>
+    if(sessionStorage.getItem("token")){
+        return(
+            <div className="flex">
+                <AdminSidebar />
+                <div className="m-auto p-10">
+                    <table className="border-collapse shadow-lg">
+                        <thead className="bg-green-400 text-white">
+                            <tr>
+                                <th className="py-5 px-10">ID Larangan Makanan</th>
+                                <th className="py-5 px-10">Nama Makanan</th>
+                                <th className="py-5 px-10">Action</th>
+                            </tr>
+                        </thead>
+                        {dataLarangan && (
+                            <tbody>
+                                {dataLarangan.map((larangan) => (
+                                    <tr className="text-center">
+                                        <td className="p-3 border-b-2">{larangan._id}</td>
+                                        <td className="p-3 border-b-2">{larangan.Makanan.Nama_Makanan}</td>
+                                        <td className="py-3 px-5 border-b-2">
+                                            <button className="p-2 bg-red-500 hover:bg-red-600 text-white rounded-md" onClick={() => {deleteLarangan(larangan._id)}}>Delete</button>
+                                        </td>
+                                    </tr>
                                 ))}
-                            </select>
+                            </tbody>
                         )}
-                        <div className="text-center mt-10">
-                            <button className="p-3 bg-green-400 text-lg text-white rounded-md font-bold" type="submit">Submit</button>
-                        </div>
-                </form>
-            </Modal>
-        </div>
-    );
+                    </table>
+                    <button className="float-right bg-green-400 p-3 text-white font-bold rounded-md mt-5" onClick={fetchMakanan}>Tambah Larangan</button>
+                </div>
+                <Modal isOpen={modalIsOpened} onRequestClose={() => setModalIsOpened(false)}>
+                    <button className="p-3 bg-red-500 text-white rounded-md float-right" onClick={() => setModalIsOpened(false)}>Close</button>
+    
+                    <h1 className="text-3xl font-bold text-black text-center mt-16">Tambah Larangan Makanan (Obesitas)</h1>
+    
+                    <form onSubmit={(e) => addLarangan(e)}>
+                        <label htmlFor="laranganMakanan" className="mt-12 inline-block text-right w-1/6 font-bold text-xl mx-5">Nama Makanan</label>
+                            {dataMakanan && (
+                                <select ref={laranganTambahan} name="laranganMakanan" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3">
+                                    
+                                    {dataMakanan.map((makanan) => (
+                                        <option value={makanan._id}>{makanan.Nama_Makanan}</option>
+                                    ))}
+                                </select>
+                            )}
+                            <div className="text-center mt-10">
+                                <button className="p-3 bg-green-400 text-lg text-white rounded-md font-bold" type="submit">Submit</button>
+                            </div>
+                    </form>
+                </Modal>
+            </div>
+        );
+    } else{
+        return(
+            <div className="text-center">
+                <h1 className="text-red-500 font-bold text-2xl p-10">You don't have access to this page!</h1>
+            </div>
+        )
+    }
+    
+    
 }
 
 export default LaranganCrud;
--- src/pages/MakananCRUD.js
+++ src/pages/MakananCRUD.js
@@ -17,6 +17,7 @@ function MakananCRUD() {
     const inputTakaran = useRef();
     const inputKalori = useRef();
 
+    const idEditMakanan = useRef();
     const editNamaMakanan = useRef();
     const editProtein = useRef();
     const editLemak = useRef();
@@ -63,15 +64,13 @@ function MakananCRUD() {
 
         const dataMakananEdited = {
             Nama_Makanan: editNamaMakanan.current.value,
-            Nilai_Protein: editProtein.current.value,
             Nilai_Lemak: editLemak.current.value,
             Nilai_Karbo: editKarbo.current.value,
+            Nilai_Protein: editProtein.current.value,
             Nilai_Takaran: editTakaran.current.value,
             Nilai_Kalori: editKalori.current.value
         }
-
-        axios.put(`https://expert-system-nutrition.herokuapp.com/api/makanan/${dataMakananSpesifik._id}`, dataMakananEdited);
-        console.log(`https://expert-system-nutrition.herokuapp.com/api/makanan/${dataMakananSpesifik._id}`)
+        console.log(dataMakananEdited);
         window.location.href = "/admin-dashboard/makanan";
     }
 
@@ -82,122 +81,130 @@ function MakananCRUD() {
         })
         setModalEditIsOpened(true);
     }
-
-    return(
-        <div className="flex">
-            <AdminSidebar />
-            <div className="m-auto p-10">
-                <table className="border-collapse shadow-lg">
-                    <thead className="bg-green-400 text-white">
-                        <tr>
-                            <th className="py-5 px-10">ID Makanan</th>
-                            <th className="py-5 px-10">Nama Makanan</th>
-                            <th className="py-5 px-10">Protein</th>
-                            <th className="py-5 px-10">Lemak</th>
-                            <th className="py-5 px-10">Karbohidrat</th>
-                            <th className="py-5 px-10">Takaran</th>
-                            <th className="py-5 px-10">Kalori</th>
-                            <th className="py-5 px-10">Action</th>
-                        </tr>
-                    </thead>
-                    {dataMakanan && (
-                        <tbody>
-                            {dataMakanan.map((makanan) => (
-                                <tr className="text-center">
-                                    <td className="p-3 border-b-2">{makanan._id}</td>
-                                    <td className="p-3 border-b-2">{makanan.Nama_Makanan}</td>
-                                    <td className="p-3 border-b-2">{makanan.Nilai_Protein}</td>
-                                    <td className="p-3 border-b-2">{makanan.Nilai_Lemak}</td>
-                                    <td className="p-3 border-b-2">{makanan.Nilai_Karbo}</td>
-                                    <td className="p-3 border-b-2">{makanan.Nilai_Takaran}</td>
-                                    <td className="p-3 border-b-2">{makanan.Nilai_Kalori}</td>
-                                    <td className="py-3 px-5 border-b-2">
-                                        <button className="py-2 px-4 mx-2 bg-yellow-400 hover:bg-yellow-500 text-white rounded-md" onClick={() => fetchMakananSpesifik(makanan._id)}>Edit</button>
-                                        <button className="p-2 bg-red-500 hover:bg-red-600 text-white rounded-md" onClick={() => deleteMakanan(makanan._id)}>Delete</button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    )}
-                </table>
-                <button onClick={() => setModalAddIsOpened(true)} className="float-right bg-green-400 p-3 text-white font-bold rounded-md mt-5">Tambah Makanan</button>
-            </div>
-
-            <Modal isOpen={modalAddIsOpened} onRequestClose={() => setModalAddIsOpened(false)}>
-                <button className="p-3 bg-red-500 text-white rounded-md float-right" onClick={() => setModalAddIsOpened(false)}>Close</button>
-                
-                <h1 className="text-3xl font-bold text-black text-center mt-16">Tambah Makanan</h1>
-
-                <form onSubmit={(e) => addMakanan(e)}>
-                    <div>
-                        <label htmlFor="namaMakanan" className="mt-12 inline-block text-right w-1/6 font-bold text-xl mx-5">Nama Makanan</label>
-                        <input type="text" ref={inputNamaMakanan} name="namaMakanan" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
-                    </div>
-                    <div>
-                        <label htmlFor="nilaiProtein" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Protein</label>
-                        <input type="text" ref={inputProtein} name="nilaiProtein" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
-                    </div>
-                    <div>
-                        <label htmlFor="nilaiLemak" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Lemak</label>
-                        <input type="text" ref={inputLemak} name="nilaiLemak" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
-                    </div>
-                    <div>
-                        <label htmlFor="nilaiKarbo" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Karbohidrat</label>
-                        <input type="text" ref={inputKarbo} name="nilaiKarbo" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
-                    </div>
-                    <div>
-                        <label htmlFor="nilaiTakaran" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Takaran</label>
-                        <input type="text" ref={inputTakaran} name="nilaiTakaran" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
-                    </div>
-                    <div>
-                        <label htmlFor="nilaiKalori" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Kalori</label>
-                        <input type="text" ref={inputKalori} name="nilaiKalori" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
-                    </div>
-                    <div className="text-center mt-10">
-                        <button className="p-3 bg-green-400 text-lg text-white rounded-md font-bold" type="submit">Submit</button>
-                    </div>
-                </form>
-            </Modal>
-
-            <Modal isOpen={modalEditIsOpened} onRequestClose={() => setModalEditIsOpened(false)}>
-                <button className="p-3 bg-red-500 text-white rounded-md float-right" onClick={() => setModalEditIsOpened(false)}>Close</button>
-                
-                <h1 className="text-3xl font-bold text-black text-center mt-16">Edit Makanan</h1>
-
-                {dataMakananSpesifik && (
-                    <form onSubmit={(e) => editMakanan(e)}>
+    if(sessionStorage.getItem("token")){
+        return(
+            <div className="flex">
+                <AdminSidebar />
+                <div className="m-auto p-10">
+                    <table className="border-collapse shadow-lg">
+                        <thead className="bg-green-400 text-white">
+                            <tr>
+                                <th className="py-5 px-10">ID Makanan</th>
+                                <th className="py-5 px-10">Nama Makanan</th>
+                                <th className="py-5 px-10">Protein</th>
+                                <th className="py-5 px-10">Lemak</th>
+                                <th className="py-5 px-10">Karbohidrat</th>
+                                <th className="py-5 px-10">Takaran</th>
+                                <th className="py-5 px-10">Kalori</th>
+                                <th className="py-5 px-10">Action</th>
+                            </tr>
+                        </thead>
+                        {dataMakanan && (
+                            <tbody>
+                                {dataMakanan.map((makanan) => (
+                                    <tr className="text-center">
+                                        <td className="p-3 border-b-2">{makanan._id}</td>
+                                        <td className="p-3 border-b-2">{makanan.Nama_Makanan}</td>
+                                        <td className="p-3 border-b-2">{makanan.Nilai_Protein}</td>
+                                        <td className="p-3 border-b-2">{makanan.Nilai_Lemak}</td>
+                                        <td className="p-3 border-b-2">{makanan.Nilai_Karbo}</td>
+                                        <td className="p-3 border-b-2">{makanan.Nilai_Takaran}</td>
+                                        <td className="p-3 border-b-2">{makanan.Nilai_Kalori}</td>
+                                        <td className="py-3 px-5 border-b-2">
+                                            <button className="py-2 px-4 mx-2 bg-yellow-400 hover:bg-yellow-500 text-white rounded-md" onClick={() => fetchMakananSpesifik(makanan._id)}>Edit</button>
+                                            <button className="p-2 bg-red-500 hover:bg-red-600 text-white rounded-md" onClick={() => deleteMakanan(makanan._id)}>Delete</button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        )}
+                    </table>
+                    <button onClick={() => setModalAddIsOpened(true)} className="float-right bg-green-400 p-3 text-white font-bold rounded-md mt-5">Tambah Makanan</button>
+                </div>
+    
+                <Modal isOpen={modalAddIsOpened} onRequestClose={() => setModalAddIsOpened(false)}>
+                    <button className="p-3 bg-red-500 text-white rounded-md float-right" onClick={() => setModalAddIsOpened(false)}>Close</button>
+                    
+                    <h1 className="text-3xl font-bold text-black text-center mt-16">Tambah Makanan</h1>
+    
+                    <form onSubmit={(e) => addMakanan(e)}>
                         <div>
                             <label htmlFor="namaMakanan" className="mt-12 inline-block text-right w-1/6 font-bold text-xl mx-5">Nama Makanan</label>
-                            <input  defaultValue={dataMakananSpesifik.Nama_Makanan} type="text" ref={editNamaMakanan} name="namaMakanan" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            <input type="text" ref={inputNamaMakanan} name="namaMakanan" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
                         </div>
                         <div>
                             <label htmlFor="nilaiProtein" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Protein</label>
-                            <input defaultValue={dataMakananSpesifik.Nilai_Protein} type="text" ref={editProtein} name="nilaiProtein" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            <input type="number" ref={inputProtein} name="nilaiProtein" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
                         </div>
                         <div>
                             <label htmlFor="nilaiLemak" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Lemak</label>
-                            <input defaultValue={dataMakananSpesifik.Nilai_Lemak} type="text" ref={editLemak} name="nilaiLemak" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            <input type="number" ref={inputLemak} name="nilaiLemak" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
                         </div>
                         <div>
                             <label htmlFor="nilaiKarbo" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Karbohidrat</label>
-                            <input defaultValue={dataMakananSpesifik.Nilai_Karbo} type="text" ref={editKarbo} name="nilaiKarbo" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            <input type="number" ref={inputKarbo} name="nilaiKarbo" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
                         </div>
                         <div>
                             <label htmlFor="nilaiTakaran" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Takaran</label>
-                            <input defaultValue={dataMakananSpesifik.Nilai_Takaran} type="text" ref={editTakaran} name="nilaiTakaran" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            <input type="number" ref={inputTakaran} name="nilaiTakaran" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
                         </div>
                         <div>
                             <label htmlFor="nilaiKalori" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Kalori</label>
-                            <input defaultValue={dataMakananSpesifik.Nilai_Kalori} type="text" ref={editKalori} name="nilaiKalori" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            <input type="number" ref={inputKalori} name="nilaiKalori" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
                         </div>
                         <div className="text-center mt-10">
                             <button className="p-3 bg-green-400 text-lg text-white rounded-md font-bold" type="submit">Submit</button>
                         </div>
                     </form>
-                )}
-            </Modal>
-        </div>
-    );
+                </Modal>
+    
+                <Modal isOpen={modalEditIsOpened} onRequestClose={() => setModalEditIsOpened(false)}>
+                    <button className="p-3 bg-red-500 text-white rounded-md float-right" onClick={() => setModalEditIsOpened(false)}>Close</button>
+                    
+                    <h1 className="text-3xl font-bold text-black text-center mt-16">Edit Makanan</h1>
+    
+                    {dataMakananSpesifik && (
+                        <form onSubmit={(e) => editMakanan(e)}>
+                            <input ref={idEditMakanan} type="hidden" defaultValue={dataMakananSpesifik._id} />
+                            <div>
+                                <label htmlFor="namaMakanan" className="mt-12 inline-block text-right w-1/6 font-bold text-xl mx-5">Nama Makanan</label>
+                                <input  defaultValue={dataMakananSpesifik.Nama_Makanan} type="text" ref={editNamaMakanan} name="namaMakanan" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            </div>
+                            <div>
+                                <label htmlFor="nilaiProtein" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Protein</label>
+                                <input defaultValue={dataMakananSpesifik.Nilai_Protein} type="number" ref={editProtein} name="nilaiProtein" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            </div>
+                            <div>
+                                <label htmlFor="nilaiLemak" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Lemak</label>
+                                <input defaultValue={dataMakananSpesifik.Nilai_Lemak} type="number" ref={editLemak} name="nilaiLemak" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            </div>
+                            <div>
+                                <label htmlFor="nilaiKarbo" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Karbohidrat</label>
+                                <input defaultValue={dataMakananSpesifik.Nilai_Karbo} type="number" ref={editKarbo} name="nilaiKarbo" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            </div>
+                            <div>
+                                <label htmlFor="nilaiTakaran" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Takaran</label>
+                                <input defaultValue={dataMakananSpesifik.Nilai_Takaran} type="number" ref={editTakaran} name="nilaiTakaran" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            </div>
+                            <div>
+                                <label htmlFor="nilaiKalori" className="mt-10 inline-block text-right w-1/6 font-bold text-xl mx-5">Nilai Kalori</label>
+                                <input defaultValue={dataMakananSpesifik.Nilai_Kalori} type="number" ref={editKalori} name="nilaiKalori" className="border w-3/5 rounded-xl focus:outline-none focus:border-green-400 py-2 px-3"/>   
+                            </div>
+                            <div className="text-center mt-10">
+                                <button className="p-3 bg-green-400 text-lg text-white rounded-md font-bold" type="submit">Submit</button>
+                            </div>
+                        </form>
+                    )}
+                </Modal>
+            </div>
+        );
+    } else{
+        return(
+            <div className="text-center">
+                <h1 className="text-red-500 font-bold text-2xl p-10">You don't have access to this page!</h1>
+            </div>
+        )
+    }
 }
 
 export default MakananCRUD;
