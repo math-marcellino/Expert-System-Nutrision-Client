6ef218a06c157817f880d80ed1891ad64357f63c
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-11-24 16:47:43 +0700

    fix: add logout functionality

--- src/Components/AdminSidebar.js
+++ src/Components/AdminSidebar.js
@@ -1,6 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function AdminSidebar() {
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        sessionStorage.removeItem("token");
+        navigate('/', { replace: true });
+    }
+
     return(
         <div className="relative min-h-screen bg-green-300 w-1/5">
             <h1 className="text-white font-bold text-3xl text-center py-5">Nutrition Admin Dashboard</h1>
@@ -22,6 +30,9 @@ function AdminSidebar() {
                     <h1 className="text-white text-center text-xl font-bold">Go to Homepage</h1>
                 </div>
             </Link>
+            <div className="w-full hover:bg-green-400 py-5 cursor-pointer">
+                <h1 onClick={handleLogout} className="text-white text-center text-xl font-bold">Log Out</h1>
+            </div>
         </div>
     );
 }
