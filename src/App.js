ae77b2a89fae0b0fbd6e0fb7a26fb8ac5a7603de
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-11-24 16:01:06 +0700

    fix: add admin register routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import AdminLogin from './pages/AdminLogin';
 import MakananCRUD from './pages/MakananCRUD';
 import LaranganCrud from './pages/LaranganCRUD';
+import AdminRegister from './pages/AdminRegister';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path="/form-diagnosa" element={<FormDiagnosa/>} />
         <Route path="/admin-login" element={<AdminLogin />}/>
+        <Route path="/admin-register" element={<AdminRegister />}/>
         <Route path="/admin-dashboard/" element={<MakananCRUD/>} />
         <Route path="/admin-dashboard/makanan" element={<MakananCRUD/>} />
         <Route path="/admin-dashboard/larangan" element={<LaranganCrud/>} />
